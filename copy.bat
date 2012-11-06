d5b3e967598a2c8eef7276ca9b53a15e04215f40
Michael Zangerle <michael.zangerle@gmail.com>
2012-11-06 11:15:05 +0100

    new libs and copy.bat

--- copy.bat
+++ copy.bat
@@ -1,1 +1,3 @@
-robocopy D:\data\development\projectsIntelli\svm\Team-C-SVM-RMI\out\artifacts\ C:\xampp\htdocs\svm /XF svm.rmi.* Team-C-SVM-RMI-Test*
+robocopy out\artifacts\ C:\xampp\htdocs\svm /XF svm.rmi.* Team-C-SVM-RMI-Test*
+
+robocopy out\artifacts\ ..\Team-C-SVM-View\lib /XF svm.rmi.implementation* Team-C-SVM-*
